ce260e070c4fe42883f85b61a8518f04b7df2087
umanhay <ptown727@yahoo.com>
2018-09-19 12:54:35 -0700

    Add links for pages

--- src/siteComponents/Header.jsx
+++ src/siteComponents/Header.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/CodeRoundsLogo.png';
 
 class Header extends Component {
     render() {
       return (
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Link to='/'><img src={logo} className="App-logo" alt="logo" /></Link>
+          <Link to='/signup'>Signup</Link>
         </header>
       );
     }
